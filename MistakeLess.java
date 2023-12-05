6febcf93b337b59e2a95e05b4d0a5a2bcc7765b9
DanialWajid <150847611+DanialWajid@users.noreply.github.com>
2023-12-06 00:41:50 +0500

    MistakeLess.java

--- MistakeLess.java
+++ MistakeLess.java
@@ -1,6 +1,8 @@
 package com.example.myfx;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.VBox;
@@ -11,23 +13,25 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
 public class MistakeLess extends Application {
-    Scene scn1, scn2, scn3, scn4,scn5,scn6;
+    Scene scn1, scn2, scn3, scn4,scn5,scn6,scn7;
+    GridPane pane7= new GridPane();
+    Stage stage;
 
 
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("Airline Management System");
         // Scene 1
-        Button button1 = new Button("Booking");
-        Button button2 = new Button("Flight Schedule");
-        Button button3 = new Button("Give Feedback");
-        Button button4= new Button(("Our reviews "));
+        Button button1 = new Button("Booking             ");
+        Button button2 = new Button("Flight Schedule ");
+        Button button3 = new Button("Give Feedback    ");
+        Button button4= new Button((   "Our reviews      "));
         GridPane pane1 = new GridPane();
         pane1.add(button1, 0, 0);
         pane1.add(button2, 0, 1);
@@ -138,22 +142,51 @@ public class MistakeLess extends Application {
 
         print.setOnAction(e ->stage.setScene(scn4));
         //scene 5 flight schedule //danial
-        Label l1 = new Label("Flight number :");
-        Label l2 = new Label("Flight Departure :");
-        Label l3 = new Label("Flight Destination :");
-        Label l4 = new Label("Flight Duration :");
+
         Button back2 = new Button("Back");
         back2.setOnAction(e ->stage.setScene(scn1));
-        GridPane pane5= new GridPane();
-        pane5.add(l1, 1, 0);
-        pane5.add(l2, 1, 1);
-        pane5.add(l3, 1, 2);
-        pane5.add(l4, 1, 3);
-        pane5.add(back2,1,4);
-        pane5.setVgap(5);
-        pane5.setHgap(5);
-        scn5 = new Scene(pane5, 300, 200);
-        button2.setOnAction(e ->stage.setScene(scn5));
+
+
+        ArrayList<Flight>flightlist= new ArrayList<>();
+        TableView tableView5 = new TableView<>();
+
+        TableColumn<Flight , String> flightno = new TableColumn<>("Flight Number");
+        flightno.setCellValueFactory(new PropertyValueFactory<>("flightNumber"));
+
+        TableColumn<Flight , String> DestinationAirport = new TableColumn<>("Destination Airport");
+        DestinationAirport.setCellValueFactory(new PropertyValueFactory<>("destinationAirport"));
+
+        TableColumn<Flight , String> DepartureAirport = new TableColumn<>("Departure Airport");
+        DepartureAirport.setCellValueFactory(new PropertyValueFactory<>("departueAirport"));
+
+        TableColumn<Flight, Date> ArrivalTime = new TableColumn<>("Arrival Time");
+        ArrivalTime.setCellValueFactory(new PropertyValueFactory<>("arrivalTime"));
+
+        TableColumn<Flight, Date> DepartureTime = new TableColumn<>("Departure Time");
+        DepartureTime.setCellValueFactory(new PropertyValueFactory<>("departureTime"));
+        ArrayList<Airport> ArrivalAirportList = Airport.getArrivalAirports();
+        ArrayList<Airport> DepartueAirportList = Airport.getDepartureAirports();
+        Date df= new Date();
+        int flightNumberCounter = 100;
+        double[] flightDuration = {1.5,2.6,3.8,9.8,15.6,17.9,20.2,5.7,5.9,8.34};
+        for (int i = 0; i < ArrivalAirportList.size(); i++) {// Assigning unique flight number and flight duration to eacg flight
+            for (int j = 0; j < DepartueAirportList.size(); j++) {
+                double duration= flightDuration[flightNumberCounter%flightDuration.length];
+                flightNumberCounter++;
+                String flightNumber = "LH" + flightNumberCounter;
+                flightlist.add(new Flight(flightNumber, df, df, duration, ArrivalAirportList.get(i).getAirportName(), DepartueAirportList.get(j).getAirportName()));
+            } }
+
+        tableView5.getColumns().addAll(flightno, DestinationAirport, DepartureAirport, ArrivalTime, DepartureTime);
+        tableView5.setItems(FXCollections.observableArrayList(flightlist));
+        VBox vBox5=new VBox(tableView5,back2);
+        scn5 = new Scene(vBox5, 300, 200);
+        button2.setOnAction(e ->{stage.setScene(scn5);
+            System.out.println(flightlist.size());
+        });
+
+
+
         //scene 6 feedback
         Label feedL1 =  new Label("Passenger Name");
         TextField tx1= new TextField();
@@ -177,7 +210,9 @@ public class MistakeLess extends Application {
         button3.setOnAction(e ->stage.setScene(scn6));
         Button feedsub= new Button("Submit");
         pane6.add(feedsub, 1, 6);
-        ArrayList<Feedback> feedbackList = new ArrayList<>();
+        TableView<Feedback> tableView = new TableView<>();
+        ArrayList<Feedback>feedbackList= new ArrayList<>();
+
         Label label61= new Label("Thank you for your feedback !");
         pane6.add(label61,1,7);
          label61.setVisible(false);
@@ -189,13 +224,9 @@ public class MistakeLess extends Application {
             feedbackList.add(new Feedback(name, content, rating, d1));
             label61.setVisible(true);
             System.out.println(feedbackList.size());
+            tableView.setItems(FXCollections.observableArrayList(feedbackList));
         });
         //scene 7 reviews
-
-        Date d2 = new Date();
-         feedbackList.add(new Feedback("danial","bad",3,d2));
-         feedbackList.add(new Feedback("wajid","verybad",9,d2));
-        TableView<Feedback> tableView = new TableView<>();
         TableColumn<Feedback, String> nameColumn = new TableColumn<>("Passenger Name");
         nameColumn.setCellValueFactory(new PropertyValueFactory<>("passengerName"));
 
@@ -214,11 +245,7 @@ public class MistakeLess extends Application {
         Scene scn7 = new Scene(vbox, 600, 400);
         button4.setOnAction(e-> stage.setScene(scn7));
         back7.setOnAction(e ->stage.setScene(scn1));
-
-
-
-
-
+        tableView.setItems(FXCollections.observableArrayList(feedbackList));
 
         //styling
         DropShadow shadow= new DropShadow();
@@ -245,4 +272,8 @@ public class MistakeLess extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-}
+
+    }
+
+
+
